686a00fa0ffe2eb80baa5300c3e8bf256dcc8f98
sashakmets <sashakmets@yandex.ru>
2015-10-27 05:48:18 +0300

    develop: рассылка 3-х дневного доступа на почту

--- inc/functions.php
+++ inc/functions.php
@@ -10,9 +10,9 @@ function registration_on_forum($first_name, $last_name, $email, $phone, $source)
     $post['source'] = $source;
     
     $post_json = json_encode($post);
-    //raport_in_files($post_json);
+    raport_in_files($post_json);
     send_mail_admin($post_json);
-    //send_mail_client($email); 
+    send_mail_client($email);
 }
 
 
@@ -36,6 +36,7 @@ function send_mail_admin($post){
         $text .= '<tr><td>'.$key.'</td><td>'.$v.'</td></tr>';
     }
     $text .= '</table>';
+
     $text .= '<p>Label: '.$_COOKIE['label'].'</p>';
 
     $mailSend = new htmlMimeMail5();
@@ -50,9 +51,25 @@ function send_mail_admin($post){
 }
 
 function send_mail_client($email){
-    $text = '<p>Спасибо за регистрацию!</p>';
-    $text.= '<p>Ждем Вас  24 и 25 марта 2015 года в НСК «Олимпийский»,"Зал Чемпионов".</p>';
-
+//    $text = '<p>Спасибо за регистрацию!</p>';
+//    $text.= '<p>Ждем Вас  24 и 25 марта 2015 года в НСК «Олимпийский»,"Зал Чемпионов".</p>';
+    $text = 'Здраствуйте!';
+    $text .= '<br/>Вы оставляли заявку на сайте <a href="http://kartina-tv.mediaplayer.com.ua/">kartina-tv.mediaplayer.com.ua</a> на получения <u>бесплатного 3х дневного доступа</u> к видео-сервису.';
+    $text .= '<br/><br/>Ваш доступ:';
+    $text .=  '<table border="1" cellpadding="10" cellspacing="0" >';
+    $text .= '<tr><td>логин</td><td><i>11448976</i></td></tr>';
+    $text .= '<tr><td>пароль</td><td><i>851422</i></td></tr>';
+    $text .= '</table>';
+    $text .= '<br/>доступ нужно активировать <b>до 07.10.2015</b>';
+    $text .= '<br/>Детальная информация о видео-сервисе: как смотреть <a href="http://kartina.tv/shop_content.php/coID/1200">kartina.tv/shop_content.php/coID/1200</a>';
+    $text .= '<br/>Если данный видео-сервис Вам понравился и Вы захотите оформить месячный или годовой доступ, обращайтесь по телефонам указанным ниже';
+    $text .= '<br/><br/>    --';
+    $text .= '<br/><i>С Уважением,';
+    $text .= '<br/>Владимир';
+    $text .= '<br/><br/>официальный диллер видео-сервиса <a href="http://kartina-tv.mediaplayer.com.ua/">Kartina.TV</a> в Украине';
+    $text .= '<br/>моб.тел. <b>+380(98) 151-30-51</b>';
+    $text .= '<br/>         <b>+38(050) 130 86 81</b>';
+    $text .= '<br/>оф.тел   <b>(044) 209 96 35</b></i></b>';
 
     $mailSend = new htmlMimeMail5();
     $helo = "Aviaforum Kiev";
@@ -60,14 +77,15 @@ function send_mail_client($email){
     $mailSend->setTextCharset('utf-8');
     $mailSend->setHtmlCharset('utf-8');
     $mailSend->setHeadCharset('utf-8');
-    $mailSend->setSubject('Авиафорум'); 
+    $mailSend->setSubject('Авиафорум');
     $mailSend->setHTML($text);
     $mailSend->send(array($email),mail);
 }
 
 function raport_in_files($post){
-    $raportFiles = "/home/aviaforum/public_html/raport/registration.log";
-    
+//    $raportFiles = "/home/aviaforum/public_html/raport/registration.log";
+    $raportFiles = "raport/registration2.log";
+
     $raportFilesOpen = fopen($raportFiles, "a+") or die("File does not exist!");
     flock($raportFilesOpen,LOCK_EX);
     foreach(json_decode($post,true) as $key => $v){ $data .= $key.':'.$v.';'; }
